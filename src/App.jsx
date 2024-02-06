0a844180c3bea769d285b719557a94bf578ad3d5
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-02-06 11:23:10 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,13 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-        <div className='navbar'>
-
-        </div>
+      <div className="container">
+        <div className="register">
+          <a href="https://forms.gle/SHxHgxk2c3WFKCgF8" className="button">Register Here</a>
+          </div>
         <div className='poster'>
           <img src={poster} alt="" />
 
-        </div>
-        <div className="register">
-            <button className="button">Register</button>
         </div>
       </div>
     </>
