00f0248dff44a0fc5d2e5f2dce72b1eafa0aa0e7
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-02-06 10:37:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import poster from './assets/AIML.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className='navbar'>
+
+        </div>
+        <div className='poster'>
+          <img src={poster} alt="" />
+
+        </div>
+        <div className="register">
+            <button className="button">Register</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
