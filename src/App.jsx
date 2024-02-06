c717ade0a6b96fecd25094be86d4660b06c71fe9
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-02-06 14:35:47 +0530

    new upadte

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
           <a href="https://forms.gle/SHxHgxk2c3WFKCgF8" className="button">Register Here</a>
           </div>
         <div className='poster'>
-          <img src={poster} alt="" />
+          <img src={poster} alt="" /> 
+          {/* poster */}
 
         </div>
         </div>
